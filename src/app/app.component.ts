d4b2afc59af06186d0cb942e63d3d6c7c4ffe0d6
Danny Van Geyte <vangeytedvg@gmail.com>
2021-07-27 15:20:14 +0200

    First Commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,10 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
+  // These become visible in app.component.html
   title = 'stock-market';
+  company = 'DenkaTech';
 }
---
+++ src/app/model/stock.ts
@@ -0,0 +1,15 @@
+// Encapsulate stocks
+export class Stock {
+  favorite: boolean = false;
+
+  constructor(
+    public name: string,
+    public code: string,
+    public price: number,
+    public previousPrice: number
+  ) {}
+
+  isPositiveChange(): boolean {
+    return this.price >= this.previousPrice;
+  }
+}
